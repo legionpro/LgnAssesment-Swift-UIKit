ce3a4b369c0dadc4b29319b8fbaeeeed5c597e5c
legionpro <poremskyy@gmail.com>
2024-06-04 19:59:35 +0300

    project basic template is ready

---
+++ LgnAssesment-Swift-UIKit/iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LgnAssesment-Swift-UIKit
+//
+//  Created by Oleh Poremskyy on 04.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .gray
+    }
+
+
+}
+
